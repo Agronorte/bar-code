2f67bc8316a3e5942f9d3a8326199da1d021c27f
agronorte <agronorte@agronorte.net>
2024-07-09 11:23:48 -0300

    serverhost

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
+const http = require('http');
+
+const hostname = '192.168.100.169'; 
+
+const port = 5500;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('dataForm');
     const resultDiv = document.getElementById('result');
